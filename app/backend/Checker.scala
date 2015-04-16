d7d4ad59beedbfb6c92b739f9fa81059dcc0f82a
slouc <sinisalouc@gmail.com>
2015-04-16 23:33:29 +0200

    added basic play frontend

---
+++ app/backend/Checker.scala
@@ -0,0 +1,31 @@
+package backend
+
+import java.io.FileNotFoundException
+
+import scala.io.Source
+
+object Checker {
+
+  private def check(url: String): Boolean = {
+    val res: Boolean = try {
+      Source.fromURL(url)
+      false
+    } catch {
+      case e: FileNotFoundException => true
+      case _: Throwable => true
+    }
+    res
+  }
+
+  def getBrokenLinks(url: String): Set[String] = {
+
+    val lines = Source.fromURL(url).getLines
+    val hrefLines = lines.flatMap(line => line.split("href=\""))
+    val httpLines = hrefLines.filter(line => line.startsWith("http"))
+    val links = httpLines.map(line => line.split("\"")(0)).toSet
+ 
+    println("Got " + links.size + " links.") 
+    
+    links.filter(check _).toSet
+  }
+}
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,40 +1,29 @@
 package controllers
 
-import org.apache.spark.SparkContext
-import play.api.mvc.Controller
-import play.api.mvc.Action
 import org.apache.spark.SparkConf
-import java.io.FileNotFoundException
-import scala.io.Source
+
+import play.api.data._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import backend.Checker
 
 object Application extends Controller {
 
-  val conf = new SparkConf().setAppName("playtest").setMaster("local")
-  val init = println("Hello")
+  //  val conf = new SparkConf().setAppName("playtest").setMaster("local")
 
   def index = Action {
-    main
     Ok(views.html.index())
   }
 
-  def check(url: String) = {
-    try {
-      Source.fromURL(url)
-    } catch {
-      case e: FileNotFoundException => println(url)
-      case _: Throwable =>
-    }
-  }
+  val form = Form(
+    "url" -> text)
 
-  def main() = {
-
-    val lines = Source.fromURL("https://github.com/onevcat/Kingfisher").getLines
-
-    val hrefLines = lines.flatMap(line => line.split("href=\""))
-    val httpLines = hrefLines.filter(line => line.startsWith("http"))
-    val links = httpLines.map(line => line.split("\"")(0))
-
-    links.foreach(check _)
+  def submit = Action { implicit request =>
+    val url = form.bindFromRequest.get
+    val links = Checker.getBrokenLinks(url)
+    Ok("RESULTS:\n\n" + links.mkString("\n"))
   }
 
 }
