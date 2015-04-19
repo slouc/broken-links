d33e310b4cfa6f6c6d48d7ec5338d9b571b27d0a
slouc <sinisalouc@gmail.com>
2015-04-19 22:33:16 +0200

    urls map to exception

--- app/backend/Checker.scala
+++ app/backend/Checker.scala
@@ -9,7 +9,7 @@ object Checker {
 
   private def open(url: String) = {
 
-    val timeout = 4000
+    val timeout = 5000
     val conn = (new URL(url)).openConnection()
     conn.setConnectTimeout(timeout)
     conn.setReadTimeout(timeout)
@@ -18,13 +18,13 @@ object Checker {
     Source.fromInputStream(inputStream)
   }
 
-  private def check(url: String, map: mutable.Map[String, String]): Boolean = {
+  private def check(url: String, map: mutable.Map[String, Exception]): Boolean = {
     val res: Boolean = try {
       open(url)
       false
     } catch {
       case e: Exception => {
-        map += url -> ("\n" + e.getMessage() + "\n")
+        map += url -> e
         true
       }
     }
@@ -40,10 +40,10 @@ object Checker {
 
     println("Got " + links.size + " links.")
 
-    val map = mutable.Map[String, String]()
+    val map = mutable.Map[String, Exception]()
 
     val filtered = links.filter(check(_, map))
-    filtered.map(link => link + map(link))
+    filtered.map(link => link + "\n" + map(link).getLocalizedMessage() + "\n")
     
   }
 }
